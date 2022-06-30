e3424e5b8426e544311d552698e3fb24c68d7b2b
Ben Haney <benjamin.haney@postmates.com>
2022-06-30 18:45:04 -0500

    Update for 103

--- build.sh
+++ build.sh
@@ -24,8 +24,7 @@ mkdir -p "$_download_cache"
 "$_main_repo/utils/prune_binaries.py" "$_src_dir" "$_main_repo/pruning.list"
 "$_main_repo/utils/patches.py" apply "$_src_dir" "$_main_repo/patches" "$_root_dir/patches"
 "$_main_repo/utils/domain_substitution.py" apply -r "$_main_repo/domain_regex.list" -f "$_main_repo/domain_substitution.list" -c "$_root_dir/build/domsubcache.tar.gz" "$_src_dir"
-cp "$_main_repo/flags.gn" "$_src_dir/out/Default/args.gn"
-cat "$_root_dir/flags.macos.gn" >> "$_src_dir/out/Default/args.gn"
+cat "$_main_repo/flags.gn" "$_root_dir/flags.macos.gn" | sort -u -t= -k1,1 > "$_src_dir/out/Default/args.gn"
 
 cd "$_src_dir"
 
